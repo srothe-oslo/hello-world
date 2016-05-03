cd4bf666bfaff553f668aef92e3cf52e219092d5
srothe-oslo <srothe-oslo@users.noreply.github.com>
2016-05-03 23:57:15 +0200

    Added files via upload

---
+++ hello.go
@@ -0,0 +1,8 @@
+//Der immerwaehrende Klassiker um eine neue Programmiersprache kennenzulernen
+package main
+import "fmt"
+func main(){
+    var myMessage string = "Moin moin, Stefan!"
+    fmt.Println("Hello World!")
+    fmt.Println(myMessage)
+}
